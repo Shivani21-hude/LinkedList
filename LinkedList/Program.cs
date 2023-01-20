5dbd89413590aafd565d2db032989bde49b7aa01
Shivani21-hude <shivani21hude@gmail.com>
2023-01-20 16:25:39 +0530

    [Shivani]-Create a simple linked list of 56,30,70

---
+++ LinkedList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace LinkedList
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Linkedlist list = new Linkedlist();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}
